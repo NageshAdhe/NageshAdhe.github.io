60a30d7555aaf5c14a696aa80fbe16807b345cdc
NageshAdhe <35455048+NageshAdhe@users.noreply.github.com>
2021-05-20 21:35:42 +0530

    except img
    except img

---
+++ precache-manifest.790d7ff4047379904939c30fe783068a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69236b31898050234e9e08eaf53f87b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49b1c1227f6bdd19a1bc",
+    "url": "/static/css/main.093b426e.chunk.css"
+  },
+  {
+    "revision": "62c99d57816b5084fb8f",
+    "url": "/static/js/2.404f1f64.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/static/js/2.404f1f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b1c1227f6bdd19a1bc",
+    "url": "/static/js/main.64fbd1e3.chunk.js"
+  },
+  {
+    "revision": "5623ad74d8e72796d082",
+    "url": "/static/js/runtime-main.0c215e12.js"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "2fe2efe63441d830b1acd106c1fe8734",
+    "url": "/static/media/Simple-Line-Icons.2fe2efe6.svg"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/static/media/Simple-Line-Icons.f33df365.eot"
+  }
+]);
